c589d9fa5cc13d79491e9b1e2a9c347c3f9aded2
KarenHernadez08 <ada_hack109@hackademy.mx>
2022-05-02 12:54:35 -0500

    Día 13

---
+++ 100 día python/dia_13.py
@@ -0,0 +1,14 @@
+""" 
+        Bienvenido al día 13 de #100díasdepython
+                El reto de hoy es:
+    Declara dos variables booleanas, evalua una expresión lógica
+        usando dos operadores lógicos e imprime el resultado
+     
+
+"""
+a=True
+b=False
+
+print(a or b, a and b)
+
+#resultado True False
