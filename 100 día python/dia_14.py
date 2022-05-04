8e173bbd25664a3a5f9bfd68388203c4885e9557
KarenHernadez08 <ada_hack109@hackademy.mx>
2022-05-04 14:34:04 -0500

    Dia 14 y 15

---
+++ 100 día python/dia_14.py
@@ -0,0 +1,13 @@
+"""
+        Bienvenido al día 14 de #100díasdepython
+                El reto de hoy es:
+    Declara una variable, mediante una expresión aritmética
+    usando dos operadores aritmeticos e imprime el resultado
+     
+
+"""
+
+x= 5*3/2
+print(x)
+
+#resultado 7.5
---
+++ 100 día python/dia_15.py
@@ -0,0 +1,12 @@
+""" 
+        Bienvenido al día 15 de #100díasdepython
+                    El reto de hoy es:
+    Declara una variable de tipo cadena e imprime los ultimos tres 
+             carcater de la cadema sin usar ciclos
+     
+"""
+
+cadena="Hola, me llamo karen"
+
+print(cadena[-3:])
+#resultado ren
