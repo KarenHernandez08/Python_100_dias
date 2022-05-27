caf8aedca4dd6d96c120cf76269ea397871027ee
KarenHernadez08 <ada_hack109@hackademy.mx>
2022-05-27 12:53:54 -0500

    día 37 y día 38

---
+++ 100 día python/dia_38.py
@@ -0,0 +1,11 @@
+""" 
+        Bienvenido al día 36 de #100díasdepython
+                    El reto de hoy es:
+    Declara un conjunto de 5 animales e imprime
+    el resultado
+     
+"""
+
+animales={'aguila', 'ballena', 'caracol', 'delfin', 'elefante'}
+
+print(animales)
